026fc96bd576f028e7746966908bac29e3d81adf
DarthByte <neverovm28@gmail.com>
2025-05-27 19:07:15 +0400

    feat: Added url

--- meteo/weather/urls.py
+++ meteo/weather/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import AutoCompleteView
+from weather.views import AutoCompleteView, CityWeatherView
 
 app_name = "weather"
 
@@ -10,4 +10,5 @@ urlpatterns = [
         AutoCompleteView.as_view(),
         name="autocomplete",
     ),
+    path("weather/<int:city_id>/", CityWeatherView.as_view(), name="city-weather"),
 ]
